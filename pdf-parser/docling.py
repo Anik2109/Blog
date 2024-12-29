ef049bbd0b151386f8dd4416ca17a2c649a7b840
Anik Kaushesh <155883621+Anik2109@users.noreply.github.com>
2024-12-30 03:20:34 +0530

    Delete pdf-parser directory

--- pdf-parser/docling.py
+++
@@ -1,7 +0,0 @@
-from docling.document_converter import DocumentConverter
-
-
-source = ""  # PDF path or URL
-converter = DocumentConverter()
-result = converter.convert(source)
-print(result.document.export_to_markdown())
